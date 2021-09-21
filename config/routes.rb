8071fc01b8e2d276504bb56611f7b26b259ce6e9
haryu <hryau6@gmail.com>
2021-09-21 20:40:39 +0900

    add: root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'groups#index'
   resources :groups do
     resource :participations, only: %i[create destroy]
     resource :roles, only: %i[update]
